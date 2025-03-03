bddddbde54377bdc958820300e2a1dc6ac596860
Alex Colville <56324439+Alexbruvv@users.noreply.github.com>
2025-03-03 23:35:13 +0000

    chore: add react compiler

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,9 +3,18 @@ import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "tailwindcss";
 
+const ReactCompilerConfig = {};
+
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [TanStackRouterVite(), react()],
+    plugins: [
+        TanStackRouterVite(),
+        react({
+            babel: {
+                plugins: ["babel-plugin-react-compiler", ReactCompilerConfig],
+            },
+        }),
+    ],
     css: {
         postcss: {
             plugins: [tailwindcss()],
