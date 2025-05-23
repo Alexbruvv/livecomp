8a570ecadc7c176013be5c964366c00e4e414b0a
Alex Colville <56324439+Alexbruvv@users.noreply.github.com>
2025-05-23 13:04:03 +0100

    fix: a second attempt at ensuring keyring bindings are bundled

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -3,19 +3,8 @@ import { version } from "../package.json";
 import { initCommand } from "./command/init";
 import { authCommand } from "./command/auth/auth";
 import { competitionCommand } from "./command/competition/competition";
-import path from "path";
-import fs from "fs";
 
-// Require keyring addon to ensure it is bundled
-const fileName = require.resolve("@napi-rs/keyring/package.json");
-const dirName = path.dirname(fileName);
-const files = fs.readdirSync(dirName);
-
-files.forEach((file) => {
-    if (file.endsWith(".node")) {
-        import(path.join(dirName, file));
-    }
-});
+// <ci-keyring-import>
 
 program
     .name("livecomp")
