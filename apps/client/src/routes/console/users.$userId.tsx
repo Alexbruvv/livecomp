0311c7d30ed8768df72e13c1aa75a64665220ef5
Alex Colville <56324439+Alexbruvv@users.noreply.github.com>
2025-05-27 13:37:30 +0100

    feat: add view user page with sessions view

--- apps/client/src/routes/console/users.$userId.tsx
+++ apps/client/src/routes/console/users.$userId.tsx
@@ -1,13 +1,48 @@
-import { ContentLayout, Header, SpaceBetween, Container, KeyValuePairs } from "@cloudscape-design/components";
+import { useCollection } from "@cloudscape-design/collection-hooks";
+import {
+    ContentLayout,
+    Header,
+    SpaceBetween,
+    Container,
+    KeyValuePairs,
+    Table,
+    Popover,
+} from "@cloudscape-design/components";
+import { authClient, User } from "@livecomp/shared";
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { useMemo } from "react";
 
 export const Route = createFileRoute("/console/users/$userId")({
     component: RouteComponent,
 });
 
 function RouteComponent() {
+    const { userId } = Route.useParams();
+    const { data: userQueryData } = useQuery({
+        queryKey: ["users", "fetch", userId],
+        queryFn: () =>
+            authClient.admin.listUsers({
+                query: {
+                    filterField: "id",
+                    filterOperator: "eq",
+                    filterValue: userId,
+                },
+            }),
+    });
+    const { data: sessionsData } = useQuery({
+        queryKey: ["users", "sessions", userId],
+        queryFn: () =>
+            authClient.admin.listUserSessions({
+                userId,
+            }),
+    });
+
+    const user = useMemo(() => userQueryData?.data?.users[0] ?? null, [userQueryData]);
+    const sessions = useMemo(() => sessionsData?.data?.sessions ?? [], [sessionsData]);
+
     return (
-        <ContentLayout header={<Header variant="h1">{"..."}</Header>}>
+        <ContentLayout header={<Header variant="h1">{user?.name ?? "..."}</Header>}>
             <SpaceBetween size="s">
                 <Container
                     header={
@@ -20,20 +55,50 @@ function RouteComponent() {
                         columns={3}
                         items={[
                             {
-                                label: "Username",
-                                value: "...",
+                                label: "Email",
+                                value: user?.email || "...",
                             },
                             {
                                 label: "Name",
-                                value: "...",
+                                value: user?.name || "...",
                             },
                             {
                                 label: "Role",
-                                value: "...",
+                                value: user?.role || "...",
                             },
                         ]}
                     />
                 </Container>
+
+                <Table
+                    header={<Header description="Click to view the user agent">Sessions</Header>}
+                    items={sessions}
+                    columnDefinitions={[
+                        {
+                            id: "id",
+                            header: "ID",
+                            cell: (session) => (
+                                <Popover
+                                    position="top"
+                                    size="medium"
+                                    content={session.userAgent ?? "Unknown user agent"}
+                                >
+                                    {session.id}
+                                </Popover>
+                            ),
+                        },
+                        {
+                            id: "createdAt",
+                            header: "Created at",
+                            cell: (session) => new Date(session.createdAt).toLocaleString(),
+                        },
+                        {
+                            id: "expiresAt",
+                            header: "Expires at",
+                            cell: (session) => new Date(session.expiresAt).toLocaleString(),
+                        },
+                    ]}
+                />
             </SpaceBetween>
         </ContentLayout>
     );
