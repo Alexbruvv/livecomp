14b199bab9899d7af941067bcf702edd86b4533e
Alex Colville <56324439+Alexbruvv@users.noreply.github.com>
2025-05-27 13:23:30 +0100

    chore: disable verbose SQL logging

--- apps/server/src/db/db.ts
+++ apps/server/src/db/db.ts
@@ -4,7 +4,7 @@ import { schema, type AppSchema } from "./schema";
 import type { AppDb } from "./repository";
 
 const connection = postgres(Bun.env.DATABASE_URL);
-export const drizzleClient = drizzle(connection, { schema, casing: "snake_case", logger: true });
+export const drizzleClient = drizzle(connection, { schema, casing: "snake_case" });
 
 export const appDb: AppDb<AppSchema> = {
     orm: drizzleClient,
