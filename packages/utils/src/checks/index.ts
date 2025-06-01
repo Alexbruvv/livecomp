0eb0073847662e3439addadff8c3f52e6d3be990
Alex Colville <56324439+Alexbruvv@users.noreply.github.com>
2025-06-01 12:48:13 +0100

    feat: add valid sequence numbers check

--- packages/utils/src/checks/index.ts
+++ packages/utils/src/checks/index.ts
@@ -3,6 +3,7 @@ import { allocatedTimeCheck } from "./allocated-time-check";
 import { matchAllocationsCheck } from "./match-allocations-check";
 import { matchPeriodOverlapsCheck } from "./match-period-overlaps-check";
 import { missingScoresCheck } from "./missing-scores-check";
+import { validSequenceNumbersCheck } from "./valid-sequence-numbers-check";
 import { withinCompetitionBoundsCheck } from "./within-competition-bounds-check";
 
 export const competitionChecks = [
@@ -12,5 +13,6 @@ export const competitionChecks = [
     allTeamsHaveLeagueMatchCheck,
     matchPeriodOverlapsCheck,
     missingScoresCheck,
+    validSequenceNumbersCheck,
 ];
 
