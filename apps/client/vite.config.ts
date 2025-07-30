b43abf9f5754b37187641dbaee46b3a179e77e38
Alex Colville <56324439+Alexbruvv@users.noreply.github.com>
2025-07-30 14:50:32 +0100

    fix: add vm module to node polyfills

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         TanStackRouterVite(),
         react(),
         nodePolyfills({
-            include: ["crypto", "stream", "util"],
+            include: ["crypto", "stream", "util", "vm"],
         }),
     ],
     css: {
